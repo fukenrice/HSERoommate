51d86036ee7f29ffd8a422c6073491dd42f2d457
arsbarss <82655677+arsbarss@users.noreply.github.com>
2022-01-29 19:50:39 +0300

    добавил приветственное сообщение

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -7,5 +7,5 @@ from states.general_states import GeneralStates
 
 @dp.message_handler(CommandStart(), state="*")
 async def bot_start(message: types.Message, state: FSMContext):
-    await message.answer(f"Привет, {message.from_user.full_name}!", reply_markup=types.ReplyKeyboardRemove())
+    await message.answer(f"Привет, {message.from_user.full_name}!\nДля того, чтобы найти идеального соседа, воспользуйся этой небольшой инструкцией:\n1) создай и заполни анкету\n2) посмотри другие анкеты и лайкни понравившиеся\n3) жди сообщения от твоего нового соседа, либо напиши ему сам (в случае, если он лайкнул тебя)\nА в случае, если что-то непонятно, не стесняйся нажимать команду help - там ты найдёшь ответы на все вопросы.\nГотов начать? Нажимай команду new_form.", reply_markup=types.ReplyKeyboardRemove())
     await GeneralStates.main_menu.set()
