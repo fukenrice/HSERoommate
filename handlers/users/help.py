0e413f398598dbd180d5c688eb488bd27b8421ef
arsbarss <82655677+arsbarss@users.noreply.github.com>
2022-01-29 19:57:14 +0300

    добавил хелп

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -7,8 +7,11 @@ from states.general_states import GeneralStates
 @dp.message_handler(CommandHelp(), state="*")
 async def bot_help(message: types.Message, state: FSMContext):
     await GeneralStates.main_menu.set()
-    text = ("Список команд: ",
-            "/start - Начать диалог",
-            "/help - Получить справку")
+    text = ("Как отредактировать анкету?",
+            "Выбери команду my_form и нажми на соответствующую кнопку.\n",
+            "Что делать, если бот лагает?",
+            "Это связано с временной перегруженностью серверов - такое бывает крайне редко. Не волнуйся, в ближайшее время всё придёт в норму.\n)",
+            "Что делать, если я не нашёл ответ на свой вопрос?",
+            "Писать в лс @ArsBars")
     
     await message.answer("\n".join(text), reply_markup=types.ReplyKeyboardRemove())
