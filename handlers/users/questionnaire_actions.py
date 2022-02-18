eb42f3852415ac9300b0f8066f01e89873870a7b
arsbarss <82655677+arsbarss@users.noreply.github.com>
2022-02-18 13:27:02 +0300

    Новые вопросы

--- handlers/users/questionnaire_actions.py
+++ handlers/users/questionnaire_actions.py
@@ -53,6 +53,82 @@ async def apply_gender(msg: types.Message, state: FSMContext):
     await state.finish()
 
 
+@dp.message_handler(lambda msg: msg.text == "Длительность проживания", state=GeneralStates.questionnaire_editing_field)
+async def change_how_long(msg: types.Message, state: FSMContext):
+    await msg.answer(text="В течение какого времени Вы хотите снимать квартиру?", reply_markup=keyboard.how_long_keyboard)
+    await GeneralStates.edited_how_long.set()
+
+
+@dp.message_handler(lambda msg: msg.text in ["Менее месяца", "От месяца до полугода", "Более полугода"], state=GeneralStates.edited_how_long)
+async def apply_how_long(msg: types.Message, state: FSMContext):
+    db.change_field('how_long', f"'{msg.text}'", telegram_id=msg.from_user.id)
+    await msg.answer(text="Длительность проживания успешно изменена", reply_markup=types.ReplyKeyboardRemove())
+    await state.finish()
+
+@dp.message_handler(lambda msg: msg.text == "Бюджет", state=GeneralStates.questionnaire_editing_field)
+async def change_budget(msg: types.Message, state: FSMContext):
+    await msg.answer(text="В Каков Ваш бюджет для аренды квартиры?", reply_markup=keyboard.budget_keyboard)
+    await GeneralStates.edited_budget.set()
+
+
+@dp.message_handler(lambda msg: msg.text in ["10-15к", "15-25к", "25-35к", "35к+"], state=GeneralStates.edited_budget)
+async def apply_budget(msg: types.Message, state: FSMContext):
+    db.change_field('budget', f"'{msg.text}'", telegram_id=msg.from_user.id)
+    await msg.answer(text="Бюджет успешно изменен", reply_markup=types.ReplyKeyboardRemove())
+    await state.finish()
+
+
+@dp.message_handler(lambda msg: msg.text == "Местоположение", state=GeneralStates.questionnaire_editing_field)
+async def change_location_global(msg: types.Message, state: FSMContext):
+    await msg.answer(text="Где бы Вы хотели жить?", reply_markup=keyboard.location_global_keyboard)
+    await GeneralStates.edited_location_global.set()
+
+
+@dp.message_handler(lambda msg: msg.text in ["Внутри ЦАО", "В пределах ТТК", "В пределах МКАД", "За МКАД", "Неважно"], state=GeneralStates.edited_location_global)
+async def apply_location_global(msg: types.Message, state: FSMContext):
+    db.change_field('location_global', f"'{msg.text}'", telegram_id=msg.from_user.id)
+    await msg.answer(text="Местоположение успешно изменено", reply_markup=types.ReplyKeyboardRemove())
+    await state.finish()
+
+
+@dp.message_handler(lambda msg: msg.text == "Местоположение (уточн.)", state=GeneralStates.questionnaire_editing_field)
+async def change_location_local(msg: types.Message, state: FSMContext):
+    await msg.answer(text="Где бы Вы хотели жить (уточн.)?", reply_markup=keyboard.location_local_keyboard)
+    await GeneralStates.edited_location_local.set()
+
+
+@dp.message_handler(lambda msg: msg.text in ["Запад", "Восток", "Север", "Юг", "Неважно"], state=GeneralStates.edited_location_local)
+async def apply_location_local(msg: types.Message, state: FSMContext):
+    db.change_field('location_local', f"'{msg.text}'", telegram_id=msg.from_user.id)
+    await msg.answer(text="Местоположение успешно изменено", reply_markup=types.ReplyKeyboardRemove())
+    await state.finish()
+
+
+@dp.message_handler(lambda msg: msg.text == "Квартира", state=GeneralStates.questionnaire_editing_field)
+async def change_apartment(msg: types.Message, state: FSMContext):
+    await msg.answer(text="Нашли ли Вы уже квартиру?", reply_markup=keyboard.binary_keyboard)
+    await GeneralStates.edited_apartment.set()
+
+
+@dp.message_handler(lambda msg: msg.text in ["Да", "Нет"], state=GeneralStates.edited_apartment)
+async def apply_apartment(msg: types.Message, state: FSMContext):
+    db.change_field('apartment', f"'{msg.text}'", telegram_id=msg.from_user.id)
+    await msg.answer(text="Данные о квартире успешно изменены", reply_markup=types.ReplyKeyboardRemove())
+    await state.finish()
+
+
+@dp.message_handler(lambda msg: msg.text == "Животные", state=GeneralStates.questionnaire_editing_field)
+async def change_pet(msg: types.Message, state: FSMContext):
+    await msg.answer(text="Готовы ли Вы жить с соседом, у которого будет домашнее животное?", reply_markup=keyboard.binary_keyboard)
+    await GeneralStates.edited_pet.set()
+
+
+@dp.message_handler(lambda msg: msg.text in ["Да", "Нет"], state=GeneralStates.edited_pet)
+async def apply_pet(msg: types.Message, state: FSMContext):
+    db.change_field('pet', f"'{msg.text}'", telegram_id=msg.from_user.id)
+    await msg.answer(text="Данные о животных успешно изменены", reply_markup=types.ReplyKeyboardRemove())
+    await state.finish()
+
 @dp.message_handler(lambda msg: msg.text == "Пол соседа", state=GeneralStates.questionnaire_editing_field)
 async def change_roommate_gender(msg: types.Message):
     await msg.answer(text="Жлеаемый пол соседа?", reply_markup=keyboard.roommate_gender_keyboard)
