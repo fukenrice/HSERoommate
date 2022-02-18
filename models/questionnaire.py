f4339919c2701b0179479763ebf0aadcecd89ad7
fukenrice <art,podoroga@yandex.ru>
2022-02-18 21:30:55 +0300

    Added new fields

--- models/questionnaire.py
+++ models/questionnaire.py
@@ -11,10 +11,21 @@ class Questionnaire:
         self.rooms_number = attributes[7]
         self.about = attributes[8]
         self.photo = attributes[9]
+        self.how_long = attributes[10]
+        self.location = attributes[11]
+        self.local_location = attributes[12]
+        self.roommate_pets = attributes[13]
+        self.budget = attributes[14]
+        self.found = attributes[15]
 
     def __str__(self):
         return f"{self.name}\n" \
                f"{self.age} лет\n" \
                f"{'Курю' if self.smoking == 1 else 'Не курю'}\n" \
+               f"Ищу квартиру {self.location}, {self.local_location}\n" \
+               f"Хочу снимать {self.how_long.lower()}\n" \
+               f"{'Против домашних животных' if self.roommate_pets == 'Нет' else 'Не против домашних животных'}\n" \
                f"Желаемое количество комнат: {self.rooms_number}\n" \
+               f"Планирую тратить примерно {self.budget}\n" \
+               f"{'Уже нашел варианты кваритр' if self.found == 'Да' else 'Пока ищу варианты квартир'}\n" \
                f"{self.about}"
