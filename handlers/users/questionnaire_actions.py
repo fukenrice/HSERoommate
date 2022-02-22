72636797725c238c2217ca7064c4b10beb32bd8c
arsbarss <82655677+arsbarss@users.noreply.github.com>
2022-02-22 21:28:01 +0300

    Update questionnaire_actions.py

--- handlers/users/questionnaire_actions.py
+++ handlers/users/questionnaire_actions.py
@@ -28,34 +28,34 @@ async def show_questionnaire(msg: types.Message):
 @dp.message_handler(lambda msg: msg.text == "Удалить анкету", state=GeneralStates.questionnaire_edit)
 async def delete_questionnaire(msg: types.Message, state: FSMContext):
     db.delete_questionnaire(msg.from_user.id)
-    await msg.answer(text="Ваша акета успешно удалена, ее больше никто не увидит."
-                          " В любой момент вы можете создать новую при помощи команды /new_form",
+    await msg.answer(text="Твоя акета успешно удалена, ее больше никто не увидит."
+                          " В любой момент ты можешь создать новую при помощи команды /new_form",
                      reply_markup=types.ReplyKeyboardRemove())
     await state.finish()
 
 
 @dp.message_handler(lambda msg: msg.text == "Редактировать анкету", state=GeneralStates.questionnaire_edit)
 async def show_edit_menu(msg: types.Message, state: FSMContext):
-    await msg.answer(text="Что вы хотите изменить?", reply_markup=keyboard.edit_questionnaire_keyboard)
+    await msg.answer(text="Что ты хочешь изменить?", reply_markup=keyboard.edit_questionnaire_keyboard)
     await GeneralStates.questionnaire_editing_field.set()
 
 
 @dp.message_handler(lambda msg: msg.text == "Пол", state=GeneralStates.questionnaire_editing_field)
 async def change_gender(msg: types.Message, state: FSMContext):
-    await msg.answer(text="Ваш пол?", reply_markup=keyboard.gender_keyboard)
+    await msg.answer(text="Твой пол?", reply_markup=keyboard.gender_keyboard)
     await GeneralStates.edited_gender.set()
 
 
 @dp.message_handler(lambda msg: msg.text in ["Муж", "Жен"], state=GeneralStates.edited_gender)
 async def apply_gender(msg: types.Message, state: FSMContext):
     db.change_field('gender', f"'{msg.text}'", telegram_id=msg.from_user.id)
-    await msg.answer(text="Ваш пол успешно изменен", reply_markup=types.ReplyKeyboardRemove())
+    await msg.answer(text="Твой пол успешно изменен", reply_markup=types.ReplyKeyboardRemove())
     await state.finish()
 
 
 @dp.message_handler(lambda msg: msg.text == "Длительность проживания", state=GeneralStates.questionnaire_editing_field)
 async def change_how_long(msg: types.Message, state: FSMContext):
-    await msg.answer(text="В течение какого времени Вы хотите снимать квартиру?", reply_markup=keyboard.how_long_keyboard)
+    await msg.answer(text="В течение какого времени ты хочешь снимать квартиру?", reply_markup=keyboard.how_long_keyboard)
     await GeneralStates.edited_how_long.set()
 
 
@@ -67,11 +67,11 @@ async def apply_how_long(msg: types.Message, state: FSMContext):
 
 @dp.message_handler(lambda msg: msg.text == "Бюджет", state=GeneralStates.questionnaire_editing_field)
 async def change_budget(msg: types.Message, state: FSMContext):
-    await msg.answer(text="В Каков Ваш бюджет для аренды квартиры?", reply_markup=keyboard.budget_keyboard)
+    await msg.answer(text="Каков твой бюджет для аренды квартиры?", reply_markup=keyboard.budget_keyboard)
     await GeneralStates.edited_budget.set()
 
 
-@dp.message_handler(lambda msg: msg.text in ["10-15к", "15-25к", "25-35к", "35к+"], state=GeneralStates.edited_budget)
+@dp.message_handler(lambda msg: msg.text in ["10-15к", "15-25к", "25-35к", "35к+", "Неважно"], state=GeneralStates.edited_budget)
 async def apply_budget(msg: types.Message, state: FSMContext):
     db.change_field('budget', f"'{msg.text}'", telegram_id=msg.from_user.id)
     await msg.answer(text="Бюджет успешно изменен", reply_markup=types.ReplyKeyboardRemove())
@@ -80,7 +80,7 @@ async def apply_budget(msg: types.Message, state: FSMContext):
 
 @dp.message_handler(lambda msg: msg.text == "Местоположение", state=GeneralStates.questionnaire_editing_field)
 async def change_location_global(msg: types.Message, state: FSMContext):
-    await msg.answer(text="Где бы Вы хотели жить?", reply_markup=keyboard.location_global_keyboard)
+    await msg.answer(text="Где бы ты хотел жить?", reply_markup=keyboard.location_global_keyboard)
     await GeneralStates.edited_location_global.set()
 
 
@@ -93,7 +93,7 @@ async def apply_location_global(msg: types.Message, state: FSMContext):
 
 @dp.message_handler(lambda msg: msg.text == "Местоположение (уточн.)", state=GeneralStates.questionnaire_editing_field)
 async def change_location_local(msg: types.Message, state: FSMContext):
-    await msg.answer(text="Где бы Вы хотели жить (уточн.)?", reply_markup=keyboard.location_local_keyboard)
+    await msg.answer(text="Где бы ты хотел жить (уточн.)?", reply_markup=keyboard.location_local_keyboard)
     await GeneralStates.edited_location_local.set()
 
 
@@ -106,7 +106,7 @@ async def apply_location_local(msg: types.Message, state: FSMContext):
 
 @dp.message_handler(lambda msg: msg.text == "Квартира", state=GeneralStates.questionnaire_editing_field)
 async def change_apartment(msg: types.Message, state: FSMContext):
-    await msg.answer(text="Нашли ли Вы уже квартиру?", reply_markup=keyboard.binary_keyboard)
+    await msg.answer(text="Нашел ли ты уже квартиру?", reply_markup=keyboard.binary_keyboard)
     await GeneralStates.edited_apartment.set()
 
 
@@ -119,7 +119,7 @@ async def apply_apartment(msg: types.Message, state: FSMContext):
 
 @dp.message_handler(lambda msg: msg.text == "Животные", state=GeneralStates.questionnaire_editing_field)
 async def change_pet(msg: types.Message, state: FSMContext):
-    await msg.answer(text="Готовы ли Вы жить с соседом, у которого будет домашнее животное?", reply_markup=keyboard.binary_keyboard)
+    await msg.answer(text="Готов ли ты жить с соседом, у которого будет домашнее животное?", reply_markup=keyboard.binary_keyboard)
     await GeneralStates.edited_pet.set()
 
 
@@ -144,7 +144,7 @@ async def apply_roommate_gender(msg: types.Message, state: FSMContext):
 
 @dp.message_handler(lambda msg: msg.text == "Имя", state=GeneralStates.questionnaire_editing_field)
 async def change_name(msg: types.Message):
-    await msg.answer(text="Введите имя", reply_markup=types.ReplyKeyboardRemove())
+    await msg.answer(text="Введи имя", reply_markup=types.ReplyKeyboardRemove())
     await GeneralStates.edited_name.set()
 
 
@@ -157,7 +157,7 @@ async def apply_name(msg: types.Message, state: FSMContext):
 
 @dp.message_handler(lambda msg: msg.text == "Возраст", state=GeneralStates.questionnaire_editing_field)
 async def change_age(msg: types.Message):
-    await msg.answer(text="Введите возраст", reply_markup=types.ReplyKeyboardRemove())
+    await msg.answer(text="Введи возраст", reply_markup=types.ReplyKeyboardRemove())
     await GeneralStates.edited_age.set()
 
 
@@ -168,17 +168,17 @@ async def apply_age(msg: types.Message, state: FSMContext):
         await msg.answer(text="Возраст успешно изменен", reply_markup=types.ReplyKeyboardRemove())
         await state.finish()
     else:
-        await msg.answer(text="Введен некорректный возраст, попробуйте снова(от 17 лет)",
+        await msg.answer(text="Введен некорректный возраст, попробуй снова(от 17 лет)",
                          reply_markup=types.ReplyKeyboardRemove())
 
 
 @dp.message_handler(lambda msg: msg.text == "Курение", state=GeneralStates.questionnaire_editing_field)
 async def change_smoking(msg: types.Message):
-    await msg.answer(text="Курите?", reply_markup=keyboard.binary_keyboard)
+    await msg.answer(text="Ты куришь?", reply_markup=keyboard.smoking_keyboard)
     await GeneralStates.edited_smoking.set()
 
 
-@dp.message_handler(lambda msg: msg.text in ["Да", "Нет"], state=GeneralStates.edited_smoking)
+@dp.message_handler(lambda msg: msg.text in ["Да", "Только электронные сигареты", "Нет"], state=GeneralStates.edited_smoking)
 async def apply_smoking(msg: types.Message, state: FSMContext):
     db.change_field('smoking', f"{(lambda: 1 if msg.text == 'Да' else 0)()}", telegram_id=msg.from_user.id)
     await msg.answer(text="Курение успешно изменено!", reply_markup=types.ReplyKeyboardRemove())
@@ -187,7 +187,7 @@ async def apply_smoking(msg: types.Message, state: FSMContext):
 
 @dp.message_handler(lambda msg: msg.text == "О себе", state=GeneralStates.questionnaire_editing_field)
 async def change_about(msg: types.Message):
-    await msg.answer(text="Расскажите о себе", reply_markup=types.ReplyKeyboardRemove())
+    await msg.answer(text="Расскажи о себе", reply_markup=types.ReplyKeyboardRemove())
     await GeneralStates.edited_about.set()
 
 
@@ -200,7 +200,7 @@ async def apply_about(msg: types.Message, state: FSMContext):
 
 @dp.message_handler(lambda msg: msg.text == "Фото", state=GeneralStates.questionnaire_editing_field)
 async def changing_photo(msg: types.Message):
-    await msg.answer(text="Отправьте мне новую фотографию", reply_markup=types.ReplyKeyboardRemove())
+    await msg.answer(text="Отправь мне новую фотографию", reply_markup=types.ReplyKeyboardRemove())
     await GeneralStates.edited_photo.set()
 
 
