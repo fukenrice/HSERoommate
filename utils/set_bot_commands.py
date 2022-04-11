18db67e8c6afb8feabed6a0acff08983a50fff5f
arsbarss <82655677+arsbarss@users.noreply.github.com>
2022-04-11 22:57:33 +0300

    вывести справку->помощь

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -5,7 +5,7 @@ async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
             types.BotCommand("start", "Запустить бота"),
-            types.BotCommand("help", "Вывести справку"),
+            types.BotCommand("help", "Помощь"),
             types.BotCommand("new_form", "Заполнить новую анкету"),
             types.BotCommand("my_form", "Посмотреть свою анкету"),
             types.BotCommand("show", "Посмотреть анкеты")
