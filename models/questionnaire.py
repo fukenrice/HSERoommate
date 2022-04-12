fa0cfbd69d1ae01a13eceb8afcfcbd7c78bc8d6f
fukenrice <63079551+fukenrice@users.noreply.github.com>
2022-04-12 22:03:57 +0300

    Pretty que view

--- models/questionnaire.py
+++ models/questionnaire.py
@@ -19,13 +19,14 @@ class Questionnaire:
         self.found = attributes[15]
 
     def __str__(self):
-        return f"{self.name}, " \
+        return f"{self.gender[0].upper()}, {self.name}, " \
                f"{self.age} {'год' if self.age % 10 == 1 else 'года' if 1 < self.age % 10 < 5 else 'лет'}\n" \
-               f"{'Курю' if self.smoking == 1 else 'Не курю'}\n" \
-               f"Ищу квартиру {'неважно где' if self.location == 'Неважно' else self.location if self.local_location == 'Неважно' else (self.location + ', ' + self.local_location)}\n" \
+               f"\N{ROUND PUSHPIN}О квартире:\n"\
+               f"Ищу квартиру {'неважно где' if self.location == 'Неважно' else self.location.lower() if self.local_location == 'Неважно' else (self.location.lower() + ', ' + self.local_location.lower())}\n" \
                f"Хочу снимать {self.how_long.lower()}\n" \
                f"{'Против домашних животных' if self.roommate_pets == 'Нет' else 'Не против домашних животных'}\n" \
-               f"Желаемое количество комнат: {self.rooms_number}\n" \
+               f"Количество комнат: {self.rooms_number.lower()}\n" \
                f"Бюджет с человека {self.budget}\n" \
-               f"{'Уже нашел варианты квартир' if self.found == 'Да' else 'Пока ищу варианты квартир'}\n\n" \
-               f"{self.about}"
+               f"{'Уже нашел варианты квартир' if self.found == 'Да' else 'Пока ищу варианты квартир'}\n" \
+               f"\N{SMOKING SYMBOL}{'Курю' if self.smoking == 1 else 'Не курю'}\n" \
+               f"\N{SPEECH BALLOON}О себе:\n{self.about}"
